dca856ea5b43b1795c678070946a03bd1bf38c20
BearWasTaken <84278620+NotBearWasTaken@users.noreply.github.com>
2023-01-10 18:24:05 +0800

    Update link.js

--- src/commands/link.js
+++ src/commands/link.js
@@ -1,7 +1,15 @@
+const { Message, Client } = require('discord.js');
+
 module.exports = {
     name: 'link',
     description: 'Sends a link to the specified repository',
-    execute(client, message, args) {
+    
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {Array} args
+     */
+    async execute(client, message, args) {
         message.channel.send('https://github.com/witheredknights');
-    },
+    }
 };
