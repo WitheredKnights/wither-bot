9a014f8a86a88cea669bb2d71b2356997991c1de
BearWasTaken <84278620+NotBearWasTaken@users.noreply.github.com>
2023-01-10 15:14:17 +0800

    Update bot token

--- resources/config.js
+++ resources/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: 'MTA1NTg2ODM5NjM0MTgyOTczMw.GU3K84.4unY3Xk2QpxNrt2jB0qxZEoxJwYTlbnEh-Ulu8',
+    token: 'MTA1NTg2ODM5NjM0MTgyOTczMw.GTkDlJ.k8rs2ItWRbxViHCsxKXqZdIziCYKgB2fBc1uiE',
     prefix: '!'
 }
