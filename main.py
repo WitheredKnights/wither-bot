0efe6baea896aa6f096770ddab1e518564078c36
prime <105910256+PrimeTDMomega@users.noreply.github.com>
2023-01-11 09:04:24 +0000

    changes to directory and added link.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import discord
 from redbot.core import commands
 from config import Config
+from scripts.config import Config
 
 bot = commands.Bot(command_prefix="-")
+
+for filename in os.listdir('./commands'):
+    if filename.endswith('.py'):
+        module = filename[:-3]
+        importlib.import_module(f'commands.{module}')
 
 def setup(bot):
     bot.load_extension("userinfo")
